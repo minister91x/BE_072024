c8216c54289e82518fb95761b6ce3eeec62215bb
quan91 <quan91.nt@gmail.com>
2024-07-17 22:21:46 +0700

    aa

--- BE_07_2024.ConsoleApp/BE_07.ConsoleApp/Program.cs
+++ BE_07_2024.ConsoleApp/BE_07.ConsoleApp/Program.cs
@@ -10,6 +10,10 @@ namespace BE_07.ConsoleApp
     {
         static void Main(string[] args)
         {
+            int myVariable = 10;
+            var myNumber = 112;
+            var myString = "MrQuan";
+
             Console.WriteLine("Xin chào các bạn");
         }
     }
